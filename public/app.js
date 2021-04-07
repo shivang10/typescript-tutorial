0487d1970f56cadd94d534958074d12114bac826
Shivang Garg <shivang10garg@gmail.com>
2021-04-07 16:35:34 +0530

    basic class added

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} - ${this.details} - ${this.amount}`;
+    }
+}
+const inOne = new Invoice("Shivang", "mytasks", 1000);
+const inTwo = new Invoice("Shiv", "mytasks", 100);
+let invoices = [];
+invoices.push(inOne, inTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} - ${this.details} - ${this.amount}`
+    }
+}
+
+const inOne = new Invoice("Shivang", "mytasks", 1000);
+const inTwo = new Invoice("Shiv", "mytasks", 100);
+
+let invoices: Invoice[] = [];
+invoices.push(inOne, inTwo);
+console.log(invoices);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 const type = document.querySelector('#type') as HTMLSelectElement;
